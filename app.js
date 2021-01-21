bec25d49e11ceaed61b4fc07a2b2e94080b51ddd
Ankit Raj <2019ugcs001@nitjsr.ac.in>
2021-01-21 18:38:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express =require('express');
 const mongoose=require('mongoose');
 const cors=require('cors');
 require('dotenv').config()
-const PORT=process.env.port||5000;
+const PORT=process.env.PORT||5000;
 // Remote db connection
 mongoose.connect(process.env.MONGOURI,{useNewUrlParser: true,useUnifiedTopology: true })
 .then(()=>{
